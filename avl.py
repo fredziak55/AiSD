8d05ffd22d9bceea01c6afae482fb0a21a05d227
fredziak55 <jan.czyz100@gmail.com>
2024-04-23 17:49:34 +0200

    added comments for tree classes

--- avl.py
+++ avl.py
@@ -1,8 +1,11 @@
 class Node:
     def __init__(self, key):
+        # Initialize a new node with the given key
         self.key = key
+        # Set the left and right children to None
         self.left = None
         self.right = None
+        # Set the height of the node to 1
         self.height = 1
         
 class AVLTree:
@@ -44,49 +47,67 @@ class AVLTree:
         n = len(nodes)
         return self.buildTreeUtil(nodes, 0, n - 1)
 
+    # Function to insert a key in the AVL tree
     def insert(self, root, key):
+        # If the tree is empty, return a new node
         if not root:
             return Node(key)
+        # If the key to be inserted is smaller than the root's key, then it goes to the left of the tree
         elif key < root.key:
             root.left = self.insert(root.left, key)
+        # If the key to be inserted is greater than the root's key, then it goes to the right of the tree
         else:
             root.right = self.insert(root.right, key)
 
-        root.height = 1 + max(self.getHeight(root.left),
-                              self.getHeight(root.right))
+        # Update the height of the ancestor node
+        root.height = 1 + max(self.getHeight(root.left), self.getHeight(root.right))
 
+        # Get the balance factor to check whether this node became unbalanced
         balance = self.getBalance(root)
 
+        # If the node is unbalanced, then there are 4 cases to handle
+
+        # Case 1 - Left Left
         if balance > 1 and key < root.left.key:
             return self.rightRotate(root)
 
+        # Case 2 - Right Right
         if balance < -1 and key > root.right.key:
             return self.leftRotate(root)
 
+        # Case 3 - Left Right
         if balance > 1 and key > root.left.key:
             root.left = self.leftRotate(root.left)
             return self.rightRotate(root)
 
+        # Case 4 - Right Left
         if balance < -1 and key < root.right.key:
             root.right = self.rightRotate(root.right)
             return self.leftRotate(root)
 
         return root
 
+     # Function to find the node with minimum key value, its always located at leftmost leaf
     def minkeyueNode(self, root):
         current = root
+        # Loop down to find the leftmost leaf
         while current.left is not None:
             current = current.left
         return current
 
+    # Function to delete a key from the tree
     def delete(self, root, key):
+        # If the tree is empty, return it
         if not root:
             return root
+        # If the key to be deleted is smaller than the root's key, then it lies in left subtree
         elif key < root.key:
             root.left = self.delete(root.left, key)
+        # If the key to be deleted is greater than the root's key, then it lies in right subtree
         elif key > root.key:
             root.right = self.delete(root.right, key)
         else:
+            # Node with only one child or no child
             if root.left is None:
                 temp = root.right
                 root = None
@@ -95,104 +116,173 @@ class AVLTree:
                 temp = root.left
                 root = None
                 return temp
+            # Node with two children, get the inorder successor (smallest in the right subtree)
             temp = self.minkeyueNode(root.right)
+            # Copy the inorder successor's content to this node
             root.key = temp.key
+            # Delete the inorder successor
             root.right = self.delete(root.right, temp.key)
+
+        # Update the height of the ancestor node
         root.height = 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+
+        # Get the balance factor to check whether this node became unbalanced
         balance = self.getBalance(root)
+
+        # If the node is unbalanced, then there are 4 cases to handle
+
+        # Case 1 - Left Left
         if balance > 1 and self.getBalance(root.left) >= 0:
             return self.rightRotate(root)
+
+        # Case 2 - Right Right
         if balance < -1 and self.getBalance(root.right) <= 0:
             return self.leftRotate(root)
+
+        # Case 3 - Left Right
         if balance > 1 and self.getBalance(root.left) < 0:
             root.left = self.leftRotate(root.left)
             return self.rightRotate(root)
+
+        # Case 4 - Right Left
         if balance < -1 and self.getBalance(root.right) > 0:
             root.right = self.rightRotate(root.right)
             return self.leftRotate(root)
+
         return root
 
     def leftRotate(self, z):
+        # Store z's right child in y
         y = z.right
+        # Store y's left child in T2
         T2 = y.left
+
+        # Perform rotation
         y.left = z
         z.right = T2
-        z.height = 1 + max(self.getHeight(z.left),
-                           self.getHeight(z.right))
-        y.height = 1 + max(self.getHeight(y.left),
-                           self.getHeight(y.right))
+
+        # Update heights
+        z.height = 1 + max(self.getHeight(z.left), self.getHeight(z.right))
+        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right))
+
+        # Return the new root
         return y
 
     def rightRotate(self, y):
+        # Store y's left child in x
         x = y.left
-        T3 = x.right
+        # Store x's right child in T2
+        T2 = x.right
+
+        # Perform rotation
         x.right = y
-        y.left = T3
-        y.height = 1 + max(self.getHeight(y.left),
-                           self.getHeight(y.right))
-        x.height = 1 + max(self.getHeight(x.left),
-                           self.getHeight(x.right))
+        y.left = T2
+
+        # Update heights
+        y.height = 1 + max(self.getHeight(y.left), self.getHeight(y.right))
+        x.height = 1 + max(self.getHeight(x.left), self.getHeight(x.right))
+
+        # Return the new root
         return x
 
     def getHeight(self, root):
+        # If the tree is empty, return 0
         if not root:
             return 0
+
+        # Otherwise, return the height of the root
         return root.height
 
     def getBalance(self, root):
+        # If the tree is empty, return 0
         if not root:
             return 0
+
+        # Otherwise, return the difference between the heights of the left and right subtrees
         return self.getHeight(root.left) - self.getHeight(root.right)
 
     def preOrder(self, root):
+        # If the tree is not empty
         if not root:
             return
+        # Print the root's key
         print("{0} ".format(root.key), end="")
+        # Recursively call preOrder on the left subtree
         self.preOrder(root.left)
+        # Recursively call preOrder on the right subtree
         self.preOrder(root.right)
     
     def minValueNode(self, root):
+        # Start at the given root
         current = root
+        # Traverse the left subtree until the leftmost node is found
         while current.left is not None:
             current = current.left
+        # Return the leftmost node (which has the minimum key)
         return current
 
     def maxValueNode(self, root):
+        # Start at the given root
         current = root
+        # Traverse the right subtree until the rightmost node is found
         while current.right is not None:
             current = current.right
+        # Return the rightmost node (which has the maximum key)
         return current
     
     def inOrder(self, root):
+        # If the tree is not empty
         if root:
+            # Recursively call inOrder on the left subtree
             self.inOrder(root.left)
+            # Print the root's key
             print(root.key, end=" ")
+            # Recursively call inOrder on the right subtree
             self.inOrder(root.right)
 
     def findMedian(self, root):
+        # If the tree is empty, return None
         if root is None:
             return None
+        # Initialize an empty list to store the nodes
         nodes = []
+        # Fill the list with the keys of the nodes in in-order traversal
         self.inOrderToList(root, nodes)
+        # Find the number of nodes
         n = len(nodes)
+        # If the number of nodes is odd, return the middle node
         if n % 2 == 1:
             return nodes[n//2]
+        # If the number of nodes is even, return the average of the two middle nodes
         else:
             return (nodes[n//2 - 1] + nodes[n//2]) / 2
 
     def inOrderToList(self, root, nodes=[]):
+        # If the tree is not empty
         if root:
+            # Recursively call inOrderToList on the left subtree
             self.inOrderToList(root.left, nodes)
+            # Add the root's key to the list
             nodes.append(root.key)
+            # Recursively call inOrderToList on the right subtree
             self.inOrderToList(root.right, nodes)
+
     def postOrder(self, root):
+        # If the tree is not empty
         if root:
+            # Recursively call postOrder on the left subtree
             self.postOrder(root.left)
+            # Recursively call postOrder on the right subtree
             self.postOrder(root.right)
-            print(root.key, end=" ")
+            # Print the root's key
+            print("{0} ".format(root.key), end="")
 
     def preOrder(self, root):
+        # If the tree is not empty
         if root:
-            print(root.key, end=" ")
+            # Print the root's key
+            print("{0} ".format(root.key), end="")
+            # Recursively call preOrder on the left subtree
             self.preOrder(root.left)
+            # Recursively call preOrder on the right subtree
             self.preOrder(root.right)
--- bst.py
+++ bst.py
@@ -1,41 +1,60 @@
 class Node:
     def __init__(self, key):
+        # Initialize a new node with the given key
         self.key = key
+        # Set the left and right children to None
         self.left = None
         self.right = None
+        # Set the height of the node to 1
         self.height = 1
         
 class BST:
     def insert(self, node, key):
+        # If the tree is empty, create a new node with the given key
         if node is None:
             return Node(key)
         else:
+            # If the given key is greater than the node's key, insert the key in the right subtree
             if node.key < key:
                 node.right = self.insert(node.right, key)
+            # Otherwise, insert the key in the left subtree
             else:
                 node.left = self.insert(node.left, key)
+        # Return the node
         return node
 
     def inorder(self, root):
+        # If the tree is not empty
         if root:
+            # Recursively call inorder on the left subtree
             self.inorder(root.left)
+            # Print the root's key
             print(root.key)
+            # Recursively call inorder on the right subtree
             self.inorder(root.right)
 
     def minkeyueNode(self, node):
+        # Start at the given node
         current = node
+        # Traverse the left subtree until the leftmost node is found
         while(current.left is not None):
             current = current.left
+        # Return the leftmost node (which has the minimum key)
         return current
 
     def delete(self, root, key):
+        # If the tree is empty, return None
         if root is None:
             return root
+        # If the key to be deleted is smaller than the root's key, then it lies in left subtree
         if key < root.key:
             root.left = self.delete(root.left, key)
+        # If the key to be deleted is greater than the root's key, then it lies in right subtree
         elif(key > root.key):
             root.right = self.delete(root.right, key)
+        # If key is same as root's key, then this is the node to be deleted
         else:
+            # Node with only one child or no child
             if root.left is None:
                 temp = root.right
                 root = None
@@ -44,77 +63,121 @@ class BST:
                 temp = root.left
                 root = None
                 return temp
+            # Node with two children: Get the inorder successor (smallest in the right subtree)
             temp = self.minkeyueNode(root.right)
+            # Copy the inorder successor's content to this node
             root.key = temp.key
+            # Delete the inorder successor
             root.right = self.delete(root.right, temp.key)
         return root
+    
     def minValueNode(self, root):
-        current = root
-        while(current.left is not None):
-            current = current.left
-        return current
+      # Start at the given root
+      current = root
+      # Traverse the left subtree until the leftmost node is found
+      while(current.left is not None):
+        current = current.left
+      # Return the leftmost node (which has the minimum key)
+      return current
 
     def maxValueNode(self, root):
+        # Start at the given root
         current = root
+        # Traverse the right subtree until the rightmost node is found
         while(current.right is not None):
             current = current.right
+        # Return the rightmost node (which has the maximum key)
         return current
 
     def inOrder(self, root):
+        # If the tree is not empty
         if root:
+            # Recursively call inOrder on the left subtree
             self.inOrder(root.left)
+            # Print the root's key
             print(root.key, end=" ")
+            # Recursively call inOrder on the right subtree
             self.inOrder(root.right)
 
     def findMedian(self, root):
+        # If the tree is empty, return None
         if root is None:
             return None
+        # Initialize an empty list to store the nodes
         nodes = []
+        # Fill the list with the keys of the nodes in in-order traversal
         self.inOrderToList(root, nodes)
+        # Find the number of nodes
         n = len(nodes)
+        # If the number of nodes is odd, return the middle node
         if n % 2 == 1:
             return nodes[n//2]
+        # If the number of nodes is even, return the average of the two middle nodes
         else:
             return (nodes[n//2 - 1] + nodes[n//2]) / 2
 
     def inOrderToList(self, root, nodes=[]):
+        # If the tree is not empty
         if root:
+            # Recursively call inOrderToList on the left subtree
             self.inOrderToList(root.left, nodes)
+            # Add the root's key to the list
             nodes.append(root.key)
+            # Recursively call inOrderToList on the right subtree
             self.inOrderToList(root.right, nodes)
 
     def postOrder(self, root):
+        # If the tree is not empty
         if root:
+            # Recursively call postOrder on the left subtree
             self.postOrder(root.left)
+            # Recursively call postOrder on the right subtree
             self.postOrder(root.right)
-            print(root.key, end=" ")
+            # Print the root's key
+            print("{0} ".format(root.key), end="")
 
     def preOrder(self, root):
+        # If the tree is not empty
         if root:
-            print(root.key, end=" ")
+            # Print the root's key
+            print("{0} ".format(root.key), end="")
+            # Recursively call preOrder on the left subtree
             self.preOrder(root.left)
+            # Recursively call preOrder on the right subtree
             self.preOrder(root.right)
 
     def to_sorted_array(self, root, arr=None):
+        # If arr is None, initialize an empty array
         if arr is None:
             arr = []
+        # If the tree is not empty
         if root:
+            # Recursively call to_sorted_array on the left subtree
             self.to_sorted_array(root.left, arr)
+            # Append the root's key to the array
             arr.append(root.key)
+            # Recursively call to_sorted_array on the right subtree
             self.to_sorted_array(root.right, arr)
+        # Return the array
         return arr
 
     def sorted_array_to_bst(self, arr):
+        # Base case: if the array is empty, return None
         if not arr:
             return None
-
+        # Find the middle element of the array
         mid = len(arr) // 2
-
+        # Create a new node with the middle element
         root = Node(arr[mid])
+        # Recursively construct the left subtree from the elements before the middle element
         root.left = self.sorted_array_to_bst(arr[:mid])
+        # Recursively construct the right subtree from the elements after the middle element
         root.right = self.sorted_array_to_bst(arr[mid+1:])
-
+        # Return the root of the constructed BST
         return root
 
     def rebalance(self, root):
-        return self.sorted_array_to_bst(self.to_sorted_array(root))
+        # Convert the BST to a sorted array
+        sorted_array = self.to_sorted_array(root)
+        # Convert the sorted array back to a balanced BST and return the root of the new BST
+        return self.sorted_array_to_bst(sorted_array)
