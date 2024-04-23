352506dfd8953d387d5eaaab662d1978801e8ce9
fredziak55 <jan.czyz100@gmail.com>
2024-04-23 17:49:55 +0200

    deleted legacy code, fixed uppercase command bug

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ def chosenTree(treeName, tree, root):
     treeName = treeName.upper()
     while True:
         command = input('command> ').lower()
+        print(command)
         if command == 'help':
             print("--- Help ---")
             print("Commands:")
@@ -103,11 +104,11 @@ def chosenTree(treeName, tree, root):
             keys = list(map(int, input('remove> ').split()))
             for key in keys:
                 root = tree.delete(root, key)
-        elif command == 'findMinMax':
+        elif command == 'findminmax':
             minTree = tree.minValueNode(root).key
             maxTree = tree.maxValueNode(root).key
             print(f"{treeName} tree: Min = {minTree}, Max = {maxTree}")
-        elif command == 'sortAndMedian':
+        elif command == 'sortandmedian':
             print(f"{treeName} tree sorted:")
             tree.inOrder(root)
             print(f"\nMedian: {tree.findMedian(root)}")
@@ -124,71 +125,6 @@ def main():
         chosenTree("AVL", avl_tree, avl_root)
     if args.bst:
         chosenTree("BST", bst_tree, bst_root)
-        # elif command == 'print':
-        #     if args.avl:
-        #         print("AVL tree:")
-        #         print("In-order:", end=" ")
-        #         avl_tree.inOrder(avl_root)
-        #         print("\nPost-order:", end=" ")
-        #         avl_tree.postOrder(avl_root)
-        #         print("\nPre-order:", end=" ")
-        #         avl_tree.preOrder(avl_root)
-        #     if args.bst:
-        #         print("BST tree:")
-        #         print("In-order:", end=" ")
-        #         bst_tree.inOrder(bst_root)
-        #         print("\nPost-order:", end=" ")
-        #         bst_tree.postOrder(bst_root)
-        #         print("\nPre-order:", end=" ")
-        #         bst_tree.preOrder(bst_root)
-        # elif command == "tickz":
-        #     if args.avl:
-        #         print("AVL tree has been saved to a txt file:")
-        #         writeTikzToFile(os.path.join(CURRENT_DIR, 'tickzpictureAVL.txt'), tikz_tree(avl_root))
-        #     if args.bst:
-        #         print("BST tree: has been saved to a txt file:")
-        #         writeTikzToFile(os.path.join(CURRENT_DIR, 'tickzpictureBST.txt'), tikz_tree(bst_root))
-        # elif command == 'insert':
-        #     num_nodes = int(input('nodes> '))
-        #     keys = list(map(int, input('insert> ').split()))
-        #     for key in keys:
-        #         if args.avl:
-        #             avl_root = avl_tree.insert(avl_root, key)
-        #         if args.bst:
-        #             bst_root = bst_tree.insert(bst_root, key)
-        # elif command == 'delete':
-        #     if args.avl:
-        #         avl_root = None
-        #     if args.bst:
-        #         bst_root = None
-        # elif command == 'rebalance':
-        #     if args.avl:
-        #         avl_root = avl_tree.rebalance(avl_root)
-        #         print("AVL tree has been balanced.")
-        #     if args.bst:
-        #         bst_root = bst_tree.rebalance(bst_root)
-        #         print("BST tree has been balanced.")
-        # elif command == 'remove':
-        #     keys = list(map(int, input('remove> ').split()))
-        #     for key in keys:
-        #         avl_root = avl_tree.delete(avl_root, key)
-        #         bst_root = bst_tree.delete(bst_root, key)
-        # elif command == 'findMinMax':
-        #     avl_min = avl_tree.minValueNode(avl_root).key
-        #     avl_max = avl_tree.maxValueNode(avl_root).key
-        #     bst_min = bst_tree.minValueNode(bst_root).key
-        #     bst_max = bst_tree.maxValueNode(bst_root).key
-        #     print(f"AVL tree: Min = {avl_min}, Max = {avl_max}")
-        #     print(f"BST tree: Min = {bst_min}, Max = {bst_max}")
-        # elif command == 'sortAndMedian':
-        #     print("AVL tree sorted:")
-        #     avl_tree.inOrder(avl_root)
-        #     print(f"\nMedian: {avl_tree.findMedian(avl_root)}")
-
-        #     print("\nBST tree sorted:")
-        #     bst_tree.inOrder(bst_root)
-        #     print(f"\nMedian: {bst_tree.findMedian(bst_root)}")
-        
 
 if __name__ == "__main__":
     main()
